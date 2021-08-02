10a77d52b7c3c5ab2597501e175aba60e5f77553
Nursahid_arya_suyudi <73198639+Nursahid20@users.noreply.github.com>
2021-08-02 10:17:15 +0700

    Update footer.php

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -18,7 +18,6 @@
 <script src="<?= BASEURL; ?>/js/popper.min.js"></script>
 <script src="<?= BASEURL; ?>/js/bootstrap.min.js"></script>
 <script src="<?= BASEURL; ?>/js/script.js"></script>
-<script src="<?= BASEURL; ?>/js/get.js"></script>
 </body>
 
 </html>
